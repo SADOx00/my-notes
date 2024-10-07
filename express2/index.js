04d741b5476400aec6473c6bb2a034d28845496d
eagle <sadeddinnahsen@gmail.com>
2024-10-07 13:19:51 +0300

    Added new GET route to index.js in express directory

--- express2/index.js
+++ express2/index.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("MiddleWar");
-  next();
+app.use("/blog/:id", (req, res) => {
+  res.send(`<h1>Detay:${req.params.id} </h1>`);
 });
 
-app.use(function (req, res, next) {
-  console.log("middlewar 3");
-  next();
+app.use("/blog", (req, res) => {
+  res.send("<h1>Blog</h1>");
 });
 
-app.use(function (req, res) {
-  console.log("Devam");
-  res.send("<h1>and ending</h1>");
+app.use("/cart", (req, res) => {
+  res.send("<h1>Cart</h1>");
+});
+
+app.use("/", (req, res) => {
+  res.send("<h1>HomePage</h1>");
 });
 
 app.listen(3000, () => {
