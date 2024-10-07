828da8376e06958eed29a2df8d40dae237f47767
eagle <sadeddinnahsen@gmail.com>
2024-10-07 11:28:45 +0300

    - Created initial directory structure for Day 1 and Lecture 2. - Added lecture materials for Day 1 and Lecture 2. - Organized files into day1 and lecture2 directories for better structure. - Included syllabus for Day 1 in the day1 directory.

---
+++ lecture2/index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const fs = require("fs");
+
+function requestListener(req, res) {
+  if (req.url === "/") {
+    fs.readFile("index.html", "utf8", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else {
+    fs.readFile("blogs.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  }
+}
+
+const app = http.createServer(requestListener);
+
+app.listen(3000);
+
+console.log("Successfully started");
