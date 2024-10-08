efb2f854c031a743e0b75f6d0060368b2240fec4
eagle <sadeddinnahsen@gmail.com>
2024-10-08 14:42:20 +0300

    bugun bitti

--- sql1/index.js
+++ sql1/index.js
@@ -1,7 +1,21 @@
 const express = require("express");
-
+const config = require("./config");
 const app = express();
 
+const mySql = require("mysql2");
+const con = mySql.createConnection(config.db);
+
+con.connect((err) => {
+  if (err) {
+    console.log(err);
+    return 0;
+  } else {
+    con.query("SELECT * FROM blogtable", (err, res) => {
+      console.log(res);
+    });
+    console.log("Connection successful");
+  }
+});
 app.set("view engine", "ejs");
 const path = require("path");
 const userRoute = require("./routes/user");
