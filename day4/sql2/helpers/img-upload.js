46ab2f1e0466c2dd8fdf8e6c7c70d41aa64717f3
eagle <sadeddinnahsen@gmail.com>
2024-10-12 12:42:39 +0300

    complete day 4

---
+++ day4/sql2/helpers/img-upload.js
@@ -0,0 +1,21 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname + "../public/images")); // Dosya yolunu kesin olarak belirt
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      path.parse(file.originalname).name +
+        "-" +
+        Date.now() +
+        path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports.upload = upload;
---
+++ day4/sql2/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+const path = require("path");
+const userRoute = require("./routes/user");
+const adminRoute = require("./routes/admin");
+
+app.use("/libs", express.static(path.join(__dirname, "node_modules")));
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoute);
+app.use(userRoute);
+
+app.listen(3000, function () {
+  console.log("listening on port 3000");
+});
